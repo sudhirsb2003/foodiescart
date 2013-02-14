b57d69fa3171ba5988bdb3ead006463b65ec75ed
sudhir <sudhir@sudhir-Satellite-A200.(none)>
2013-02-14 18:11:27 +0530

    active admin corrected

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class DashboardsController < ApplicationController
 
  def index   
-
+  @recipes = Recipe.last(4)
 if user_signed_in? 
 @user = current_user.id
 @wallet = Wallet.find_all_by_user_id(@user)
